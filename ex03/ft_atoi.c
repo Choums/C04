7fc7810d8d40302a3e77808e8dafd5ef67cabd95
Chams <chams.aidel@outlook.fr>
2020-07-12 15:32:27 +0200

    1left

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,34 +6,42 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 20:01:53 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/10 16:18:48 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/12 15:31:50 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_stoi(char *str)
+int	is_whitespace_true(char *str)
 {
+	int i;
+
+	i = 0;
+	if (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		return (1);
+
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{	
 	int res;
 	int sign;
 	int i;
 
 	res = 0;
-	sign = -1;
+	sign = 1;
 	i = 0;
 
-	/*
-	 * Reste a prendre en compte les whitespaces
-	 */
-	while (/*whitespace*/)
-		i++
+	while (is_whitespace_true(str))
+		i++;
 
 	while (str[i] == '-' || str[i] == '+')
 	{
-		if (str[0] == '-')
+		if (str[i] == '-')
 		{
 			sign *= -1;
-			i++;
 		}
-
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res*10 + (str[i] - '0');
