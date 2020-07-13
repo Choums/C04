5e3f1d7364be0ec977da3b867b4fd588974e580e
Chams <chams.aidel@outlook.fr>
2020-07-13 11:05:56 +0200

    ex3

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 20:01:53 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/12 15:31:50 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/13 10:48:51 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@ int	is_whitespace_true(char *str)
 	int i;
 
 	i = 0;
-	if (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	if (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+			|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		return (1);
-
 	return (0);
 }
 
 int	ft_atoi(char *str)
-{	
+{
 	int res;
 	int sign;
 	int i;
@@ -30,10 +30,8 @@ int	ft_atoi(char *str)
 	res = 0;
 	sign = 1;
 	i = 0;
-
-	while (is_whitespace_true(str))
+	while (is_whitespace_true(str + i))
 		i++;
-
 	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
@@ -44,8 +42,8 @@ int	ft_atoi(char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		res = res*10 + (str[i] - '0');
+		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-	return (sign*res);
+	return (sign * res);
 }
